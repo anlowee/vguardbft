20ab54a30c042241f80c75ff9d9d7b49684b83d2
xiaochong.wei <anlowee@outlook.com>
2023-04-06 08:16:43 -0400

    finish the simulating

--- web/src/Map.js
+++ web/src/Map.js
@@ -1,6 +1,37 @@
 import React, {useCallback, useEffect, useState} from "react";
 import Maps, {computeDistance, interpolate} from "react-maps-suite";
 
+const defaultCenter = {
+  lat: 43.394452,
+  lng: -80.493500
+};
+
+const ranch2FarmPath = [
+  { lat: 43.645152, lng: -79.374808 },
+  { lat: 43.440998, lng: -80.488945 },
+  { lat: 42.969881, lng: -81.245908 },
+].reduce(reducePath, []);
+
+const farm2RanchPath = [
+  { lat: 42.969881, lng: -81.245908 },
+  { lat: 43.440998, lng: -80.488945 },
+  { lat: 43.645152, lng: -79.374808 },
+].reduce(reducePath, []);
+
+const farm2CanneryPath = [
+  { lat: 42.969881, lng: -81.245908 },
+  { lat: 42.868654, lng: -80.308678 },
+  { lat: 42.954596, lng: -79.855055 },
+].reduce(reducePath, []);
+
+const ranch2CanneryPath = [
+  { lat: 43.645152, lng: -79.374808 },
+  { lat: 43.248315, lng: -79.886457 },
+  { lat: 42.954596, lng: -79.855055 },
+].reduce(reducePath, []);
+
+const defaultZoom= 8;
+
 function getPositionAt(path, distance) {
   const indexesPassed = path.filter((position) => position.distance < distance);
   if (indexesPassed.length === 0) {
@@ -27,16 +58,7 @@ function getPositionAt(path, distance) {
   );
 }
 
-const defaultCenter = {
-  lat: 43.394452,
-  lng: -80.493500
-};
-
-const defaultPath = [
-  { lat: 44.752347, lng: -79.495547 },
-  { lat: 44.241208, lng: -82.268736 },
-  { lat: 43.099234, lng: -84.163449 },
-].reduce((result, item, index, array) => {
+function reducePath(result, item, index, array) {
   if (index === 0) {
     result.push({ ...item, distance: 0 });
     return result;
@@ -48,13 +70,38 @@ const defaultPath = [
 
   result.push({ ...item, distance });
   return result;
-}, []);
-
-const defaultZoom= 6;
-
-const DEFAULT_SPEED= 100; // m/s
+}
 
 function Map() {
+  const [trucksFromRanchToFarm, setTrucksFromRanchToFarm] = useState([0, 0, 0]);
+  const [trucksFromFarmToRanch, setTrucksFromFarmToRanch] = useState([0, 0, 0]);
+  const [trucksFromFarmToCannery, setTrucksFromFarmToCannery] = useState([0, 0, 0]);
+  const [trucksFromRanchToCannery, setTrucksFromRanchToCannery] = useState([0, 0, 0]);
+  const updateTrucksFromRanchToFarm = useCallback((truckNumber, distance) => {
+    setTrucksFromRanchToFarm(trucksFromRanchToFarm => {
+      trucksFromRanchToFarm[truckNumber] = distance;
+      return [...trucksFromRanchToFarm];
+    });
+  }, []);
+  const updateTrucksFromFarmToRanch = useCallback((truckNumber, distance) => {
+    setTrucksFromFarmToRanch(trucksFromFarmToRanch => {
+      trucksFromFarmToRanch[truckNumber] = distance;
+      return [...trucksFromFarmToRanch];
+    });
+  }, []);
+  const updateTrucksFromFarmToCannery = useCallback((truckNumber, distance) => {
+    setTrucksFromFarmToCannery(trucksFromFarmToCannery => {
+      trucksFromFarmToCannery[truckNumber] = distance;
+      return [...trucksFromFarmToCannery];
+    });
+  }, []);
+  const updateTrucksFromRanchToCannery = useCallback((truckNumber, distance) => {
+    setTrucksFromRanchToCannery(trucksFromRanchToCannery => {
+      trucksFromRanchToCannery[truckNumber] = distance;
+      return [...trucksFromRanchToCannery];
+    });
+  }, []);
+
   const [time, setTime] = useState(0);
   const increaseTime = useCallback(() => {
     setTime(time => time + 1);
@@ -67,8 +114,27 @@ function Map() {
     };
   }, [increaseTime]);
 
-  const distance = DEFAULT_SPEED * time;
-  const position = getPositionAt(defaultPath, distance);
+  useEffect(() => {
+    for (let i = 0; i < 3; i++) {
+      const speedOfTruckFromRanchToFarm = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+      const speedOfTruckFromFarmToRanch = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+      const speedOfTruckFromFarmToCannery = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+      const speedOfTruckFromRanchToCannery = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+
+      updateTrucksFromRanchToFarm(i, trucksFromRanchToFarm[i] + speedOfTruckFromRanchToFarm);
+      updateTrucksFromFarmToRanch(i, trucksFromFarmToRanch[i] + speedOfTruckFromFarmToRanch);
+      updateTrucksFromFarmToCannery(i, trucksFromFarmToCannery[i] + speedOfTruckFromFarmToCannery);
+      updateTrucksFromRanchToCannery(i, trucksFromRanchToCannery[i] + speedOfTruckFromRanchToCannery);
+    }
+  }, [time]);
+
+  const markers = [];
+  for (let i = 0; i < 3; i++) {
+    markers.push(<Maps.Marker key={i} position={getPositionAt(ranch2FarmPath, trucksFromRanchToFarm[i])} />);
+    markers.push(<Maps.Marker key={i + 3} position={getPositionAt(farm2RanchPath, trucksFromFarmToRanch[i])} />);
+    markers.push(<Maps.Marker key={i + 6} position={getPositionAt(farm2CanneryPath, trucksFromFarmToCannery[i])} />);
+    markers.push(<Maps.Marker key={i + 9} position={getPositionAt(ranch2CanneryPath, trucksFromRanchToCannery[i])} />);
+  }
 
   return (
     <Maps
@@ -77,7 +143,10 @@ function Map() {
       defaultCenter={defaultCenter}
       defaultZoom={defaultZoom}
     >
-      <Maps.Marker position={position} />
+      { markers }
+      <Maps.Polyline path={ranch2CanneryPath} strokeColor="#fc0a08" />
+      <Maps.Polyline path={farm2CanneryPath} strokeColor="#0868fc" />
+      <Maps.Polyline path={ranch2FarmPath} strokeColor="#fc9c08" />
     </Maps>
   );
 }
